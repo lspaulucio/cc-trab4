239a18a65e4e3774796438a752061e10cd790b89
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2016-11-30 15:09:49 -0200

    First 10 examples working

--- interpreter.c
+++ interpreter.c
@@ -14,16 +14,21 @@ extern LitTable *lt;
 int stack[STACK_SIZE];
 int sp; // stack pointer
 
-void push(int x) {
+void push(int x)
+{
     stack[++sp] = x;
 }
 
-int pop() {
+int pop()
+{
     return stack[sp--];
 }
 
-void init_stack() {
-    for (int i = 0; i < STACK_SIZE; i++) {
+void init_stack()
+{
+    int i;
+    for (i = 0; i < STACK_SIZE; i++)
+    {
         stack[i] = 0;
     }
     sp = -1;
@@ -31,7 +36,9 @@ void init_stack() {
 
 void print_stack() {
     printf("*** STACK: ");
-    for (int i = 0; i <= sp; i++) {
+    int i;
+    for (i = 0; i <= sp; i++)
+    {
         printf("%d ", stack[i]);
     }
     printf("\n");
@@ -45,16 +52,21 @@ void print_stack() {
 
 int mem[MEM_SIZE];
 
-void store(int addr, int val) {
+void store(int addr, int val)
+{
     mem[addr] = val;
 }
 
-int load(int addr) {
+int load(int addr)
+{
     return mem[addr];
 }
 
-void init_mem() {
-    for (int addr = 0; addr < MEM_SIZE; addr++) {
+void init_mem()
+{
+    int addr;
+    for (addr = 0; addr < MEM_SIZE; addr++)
+    {
         mem[addr] = 0;
     }
 }
@@ -162,36 +174,24 @@ void run_input(AST *ast)
 {
     trace("input");
 
-    int i, size = get_child_count(ast);
-
-    for (i = 0; i < size; i++)
-    {
-        rec_run_ast(get_child(ast, i));
-    }
+    int num;
+    scanf("%d", &num);
+    push(num);
 }
 
 void run_output(AST *ast)
 {
     trace("output");
 
-    int i, size = get_child_count(ast);
-
-    for (i = 0; i < size; i++)
-    {
-        rec_run_ast(get_child(ast, i));
-    }
+    rec_run_ast(get_child(ast, 0));
+    int x = pop();
+    printf("%d\n", x);
 }
 
 void run_svar(AST *ast)
 {
     trace("svar");
-
-    int i, size = get_child_count(ast);
-
-    for (i = 0; i < size; i++)
-    {
-        rec_run_ast(get_child(ast, i));
-    }
+    push(load(getPos(ast)));
 }
 
 void run_cvar(AST *ast)
@@ -211,9 +211,11 @@ void run_if(AST *ast)
     trace("if");
     rec_run_ast(get_child(ast, 0));
     int test = pop();
-    if (test == 1) {
+    if (test == 1)
+    {
         rec_run_ast(get_child(ast, 1));
-    } else if (test == 0 && get_child_count(ast) == 3) {
+    } else if (test == 0 && get_child_count(ast) == 3)
+    {
         rec_run_ast(get_child(ast, 2));
     }
 }
@@ -249,12 +251,23 @@ void run_read(AST *ast)
     store(var_idx, x);
 }
 
+// char* process_string(char* str)
+// {
+//     char *buffer = str;
+//     printf("%s", str);
+//     sscanf(str, "\"%[^\"]s", buffer);
+//
+//     return buffer;
+// }
+
 void run_write(AST *ast)
 {
     trace("write");
     rec_run_ast(get_child(ast, 0));
     int x = pop();
-    printf("%s\n", get_literal(lt, x));
+    char *string = get_literal(lt, x);
+    // printf("%s\n", process_string(string));
+    printf("%s\n", string);
 }
 
 void run_string(AST *ast)
@@ -276,7 +289,14 @@ void run_header(AST *ast)
 
 void run_while(AST *ast)
 {
-    return;
+    rec_run_ast(get_child(ast, 0));
+    int test = pop();
+    while (test == 1)
+    {
+        rec_run_ast(get_child(ast, 1));
+        rec_run_ast(get_child(ast, 0));
+        test = pop();
+    }
 }
 
 void run_arg_list(AST *ast)
@@ -322,6 +342,34 @@ void run_lt(AST *ast)
     trace("lt");
     bin_op();
     push(l < r);
+}
+
+void run_le(AST *ast)
+{
+    trace("le");
+    bin_op();
+    push(l <= r);
+}
+
+void run_gt(AST *ast)
+{
+    trace("gt");
+    bin_op();
+    push(l > r);
+}
+
+void run_ge(AST *ast)
+{
+    trace("ge");
+    bin_op();
+    push(l >= r);
+}
+
+void run_neq(AST *ast)
+{
+    trace("neq");
+    bin_op();
+    push(l != r);
 }
 
 void run_eq(AST *ast)
@@ -422,6 +470,18 @@ void rec_run_ast(AST *ast)
             break;
         case LT_NODE:
             run_lt(ast);
+            break;
+        case LE_NODE:
+            run_le(ast);
+            break;
+        case GT_NODE:
+            run_gt(ast);
+            break;
+        case GE_NODE:
+            run_ge(ast);
+            break;
+        case NEQ_NODE:
+            run_neq(ast);
             break;
         case EQ_NODE:
             run_eq(ast);
